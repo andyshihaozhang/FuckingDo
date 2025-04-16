a04edafd666d0a33d09628c56d07381869431193
andyshihaozhang <andyshihaozhang@gmail.com>
2025-04-16 13:22:20 +0800

    提交app主窗口-添加设置page、侧边菜单栏

---
+++ App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using FuckingDo.Resources;
+using FuckingDo.Services;
+using FuckingDo.Services.Contracts;
+using FuckingDo.ViewModels.Pages;
+using FuckingDo.ViewModels.Windows;
+using FuckingDo.Views.Pages;
+using FuckingDo.Views.Windows;
+using Lepo.i18n.DependencyInjection;
+using Wpf.Ui;
+using Wpf.Ui.DependencyInjection;
+
+namespace FuckingDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // 依赖注入配置-添加相关服务
+        private static readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(c =>
+        {
+            _ = c.SetBasePath(AppContext.BaseDirectory);
+        })
+        .ConfigureServices(
+            (_1, services) =>
+            {
+                _ = services.AddNavigationViewPageProvider();
+
+                // App Host
+                _ = services.AddHostedService<ApplicationHostService>();
+
+                // Main window container with navigation
+                _ = services.AddSingleton<IWindow, MainWindow>();
+                _ = services.AddSingleton<MainWindowViewModel>();
+                _ = services.AddSingleton<INavigationService, NavigationService>();
+
+                // Top-level pages
+                _ = services.AddSingleton<SettingsPage>();
+                _ = services.AddSingleton<SettingsViewModel>();
+
+                _ = services.AddStringLocalizer(b =>
+                {
+                    b.FromResource<Translations>(new("pl-PL"));
+                });
+            }
+            ).Build();
+
+        /// <summary>
+        /// Gets registered service.
+        /// </summary>
+        /// <typeparam name="T">Type of the service to get.</typeparam>
+        /// <returns>Instance of the service or <see langword="null"/>.</returns>
+        public static T GetRequiredService<T>()
+            where T : class
+        {
+            return _host.Services.GetRequiredService<T>();
+        }
+
+        /// <summary>
+        /// Occurs when the application is loading.
+        /// </summary>
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            _host.Start();
+        }
+
+        /// <summary>
+        /// Occurs when the application is closing.
+        /// </summary>
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            _host.StopAsync().Wait();
+
+            _host.Dispose();
+        }
+    }
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FuckingDo.Services.Contracts;
+using Wpf.Ui;
+using FuckingDo.ViewModels.Windows;
+
+namespace FuckingDo.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : IWindow
+    {
+        public MainWindowViewModel ViewModel { get; }
+
+
+        public MainWindow(
+            MainWindowViewModel viewModel,
+            INavigationService navigationService)
+        {
+            Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
+
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+
+            navigationService.SetNavigationControl(NavigationView);
+
+        }
+    }
+}
